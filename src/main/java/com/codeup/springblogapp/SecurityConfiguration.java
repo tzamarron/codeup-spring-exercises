f84e23ededd5a11a4c9a61f30cd00bd1a0b7911b
Ted Zamarron <ted.zamarron@gmail.com>
2020-05-18 17:43:28 -0500

    modification

--- src/main/java/com/codeup/springblogapp/SecurityConfiguration.java
+++ src/main/java/com/codeup/springblogapp/SecurityConfiguration.java
@@ -46,14 +46,14 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 /* Pages that can be viewed without having to log in */
                 .and()
                 .authorizeRequests()
-                .antMatchers("/", "/posts") // anyone can see the home and the ads pages
+                .antMatchers("/", "/posts") // anyone can see the home and the posts pages
                 .permitAll()
                 /* Pages that require authentication */
                 .and()
                 .authorizeRequests()
                 .antMatchers(
-                        "/posts/{id}}", // only authenticated users can view posts
-                        "/posts/createPost}", // only authenticated users can create posts
+//                        "/posts/{id}", // only authenticated users can view posts
+                        "/posts/createPost", // only authenticated users can create posts
                         "/posts/{id}/edit", // only authenticated users can edit posts
                         "/posts/delete" // only authenticated users can delete posts
                 )
